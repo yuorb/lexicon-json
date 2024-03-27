2a28648b68605934c01c64e5d62b2712ce886feb
Verte <vertesian@protonmail.com>
2024-03-27 09:51:50 +0530

    apply consistent formatting

--- src/types.ts
+++ src/types.ts
@@ -1,72 +1,72 @@
 type Specs = {
-    "BSC": string,
-    "CTE": string,
-    "CSV": string,
-    "OBJ": string
-}
+  "BSC": string;
+  "CTE": string;
+  "CSV": string;
+  "OBJ": string;
+};
 
 export type Root = {
-    root: string,
-    refers: string,
-    stems?: [
-        Specs | string,
-        Specs | string,
-        Specs | string,
-    ],
-    notes?: string
+  root: string;
+  refers: string;
+  stems?: [
+    Specs | string,
+    Specs | string,
+    Specs | string,
+  ];
+  notes?: string;
 };
 
 type Degree = string | [string, string];
 
 export type StandardAffix = {
-    name: string;
-    description: string;
-    gradient_type: "0" | "A1" | "A2" | "B" | "C" | "D1" | "D2";
-    cs: string;
-    degrees: [
-        Degree | null,
-        Degree,
-        Degree,
-        Degree,
-        Degree,
-        Degree,
-        Degree,
-        Degree,
-        Degree,
-        Degree,
-    ];
-    notes?: string;
+  name: string;
+  description: string;
+  gradient_type: "0" | "A1" | "A2" | "B" | "C" | "D1" | "D2";
+  cs: string;
+  degrees: [
+    Degree | null,
+    Degree,
+    Degree,
+    Degree,
+    Degree,
+    Degree,
+    Degree,
+    Degree,
+    Degree,
+    Degree,
+  ];
+  notes?: string;
 };
 
 type Case = {
-    cs: string;
-    vx: Array<string>;
-    description: string;
+  cs: string;
+  vx: Array<string>;
+  description: string;
 };
 
 export type CaseAccessorAffix = {
-    name: string;
-    description: string;
-    gradient_type: string;
-    types: [
-        Array<Case>,
-        Array<Case>,
-        Array<Case>,
-    ];
+  name: string;
+  description: string;
+  gradient_type: string;
+  types: [
+    Array<Case>,
+    Array<Case>,
+    Array<Case>,
+  ];
 };
 
 export type CaseStackingAffix = {
-    name: string;
-    description: string;
-    gradient_type: string;
-    cases: Array<Case>;
+  name: string;
+  description: string;
+  gradient_type: string;
+  cases: Array<Case>;
 };
 
 export type Lexicon = {
-    roots: Root[],
-    affixes: {
-        standard: StandardAffix[],
-        accessor: CaseAccessorAffix[],
-        stacking: CaseStackingAffix[],
-    },
-}
+  roots: Root[];
+  affixes: {
+    standard: StandardAffix[];
+    accessor: CaseAccessorAffix[];
+    stacking: CaseStackingAffix[];
+  };
+};
